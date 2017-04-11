264d9998234ae0f453488a3bb9d1ab12dfb07720
Hack Reactor Student <student@hackreactor.com>
2017-04-10 17:32:06 -0700

    Made a couple of tests pass.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,21 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+	this.on('add', function(){
+		if(this.length === 1){
+			this.playFirst();
+		}
+	})
+	
+	this.on('ended', function () {
+		this.shift();
+	})
+  },
+
+  playFirst: function(){
+  	//this.model.play();
+  },
+
 
 });
+
